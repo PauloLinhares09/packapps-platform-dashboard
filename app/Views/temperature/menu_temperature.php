fc33ff34d889e16a990878f537825907bf8b69dc
Paulo Linhares <inf00174@sky.com.br>
2024-01-25 16:38:29 -0300

    menu identify

--- app/Views/temperature/menu_temperature.php
+++ app/Views/temperature/menu_temperature.php
@@ -23,19 +23,45 @@
             display: inline;
         }
 
+        .item-ativo {
+            border-left: 4px solid #007bff; /* Cor da borda */    
+        }
+
     </style>
+
+
+<?php 
+    $request = \Config\Services::request();
+    $uriPath = $request->getPath();
+
+    $leads = false;
+    $clients = false;
+    $campaigns = false;
+    $sendMessage = false;
+    
+    if ($uriPath === 'temperature/leads') {
+        $leads = true;
+    } else if ($uriPath === 'temperature/clients') {
+        $clients = true;
+    } else if ($uriPath === 'temperature/campaigns') {
+        $campaigns = true;
+    } else if ($uriPath === 'temperature/sendMessage') {
+        $sendMessage = true;
+    } 
+       
+?>
 
     
         
     <!-- Content here -->
     <div id="sidebar-submenu">        
-        <a href="<?= base_url('/temperature/leads') ?>"><img src="../assets/robot_2_FILL0_wght400_GRAD0_opsz24.png" alt="Leads">Leads</a>
-        <a href="<?= base_url('/temperature/clients') ?>"><img src="../assets/ar_stickers_FILL0_wght400_GRAD0_opsz24.png" alt="Clientes">Clientes</a>
+        <a href="<?= base_url('/temperature/leads') ?>" <?= $leads == true ? "class=\"item-ativo\"" : "" ?> ><img src="../assets/robot_2_FILL0_wght400_GRAD0_opsz24.png" alt="Leads">Leads</a>
+        <a href="<?= base_url('/temperature/clients') ?>" <?= $clients == true ? "class=\"item-ativo\"" : "" ?>><img src="../assets/ar_stickers_FILL0_wght400_GRAD0_opsz24.png" alt="Clientes">Clientes</a>
 
         <div style="height: 5px; margin-bottom: 30px; border-bottom: 1px solid #eaecef;"></div>
         
-        <a href="<?= base_url('/temperature/campaigns') ?>"><img src="../assets/robot_2_FILL0_wght400_GRAD0_opsz24.png" alt="Leads">Campanhas</a>
-        <a href="<?= base_url('/temperature/sendMessage') ?>"><img src="../assets/robot_2_FILL0_wght400_GRAD0_opsz24.png" alt="Leads">Mensagem em massa</a>
+        <a href="<?= base_url('/temperature/campaigns') ?>" <?= $campaigns == true ? "class=\"item-ativo\"" : "" ?>><img src="../assets/robot_2_FILL0_wght400_GRAD0_opsz24.png" alt="Leads">Campanhas</a>
+        <a href="<?= base_url('/temperature/sendMessage') ?>" <?= $sendMessage == true ? "class=\"item-ativo\"" : "" ?>><img src="../assets/robot_2_FILL0_wght400_GRAD0_opsz24.png" alt="Leads">Mensagem em massa</a>
     </div>
     
 
